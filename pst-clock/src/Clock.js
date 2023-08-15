80a88d489ccfa6b5a96de37ddd07125a8c6c957a
ryandaepark <ryan@moonbeam.ai>
2023-08-15 11:23:44 -0700

    Complete

--- pst-clock/src/Clock.js
+++ pst-clock/src/Clock.js
@@ -1,10 +1,20 @@
-import React from 'react'
-
+import { useState } from 'react';
+import './clock.css';
 const Clock = () => {
     let time = new Date().toLocaleTimeString();
- 
+    const [currentTime, setCurrentTime] = useState(time);
+
+    const updateTime = () => {
+        time = new Date().toLocaleTimeString();
+        setCurrentTime(time);
+    }
+
+    setInterval(updateTime, 1000); //1000 is 1sec 
+
     return (
-        <h1>{time}</h1>
+        <div className="clock">
+            <h1>{currentTime}</h1>
+        </div>
     )
 }
 
